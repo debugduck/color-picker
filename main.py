79237317ad7421ba4ec7332760ddcf860103758c
Claire <clairekcecil@gmail.com>
2017-08-08 00:47:30 -0400

    Added a top nav bar and moved html to templates directory

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ import os
 class ColorPicker(object):
 	@cherrypy.expose
 	def index(self):
-		return open('color_picker.html')
+		return open('templates/color_picker.html')
 	
 if __name__ == '__main__':
 	conf = {
 		'/': {
-            'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
-        },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': 'images'
-        },
+		    'tools.sessions.on': True,
+		    'tools.staticdir.root': os.path.abspath(os.getcwd())
+		},
+		'/templates': {
+		    'tools.staticdir.on': True,
+		    'tools.staticdir.dir': 'templates'
+		},
+		'/static': {
+		    'tools.staticdir.on': True,
+		    'tools.staticdir.dir': 'images'
+		},
 		'/js': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': 'js'
-        }
+		    'tools.staticdir.on': True,
+		    'tools.staticdir.dir': 'js'
+		}
 	}
 	cherrypy.quickstart(ColorPicker(), '/', conf)
