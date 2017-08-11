1f3a48311b5db8d9f9ad44259d8705e57883f9d8
Debug Duck <debugduck@users.noreply.github.com>
2017-08-11 02:09:07 +0000

    Moved conf to separate file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment( loader=FileSystemLoader('templates' ))
 
-class Main(object):
+class Root(object):
 
 	@cherrypy.expose
 	def rgb(self):
@@ -17,23 +17,8 @@ class Main(object):
 	        template = env.get_template('color_picker.html')
 		return template.render()
 	
-if __name__ == '__main__':
-	conf = {
-		'/': {
-		    'tools.sessions.on': True,
-		    'tools.staticdir.root': os.path.abspath(os.getcwd())
-		},
-		'/templates': {
-		    'tools.staticdir.on': True,
-		    'tools.staticdir.dir': 'templates'
-		},
-		'/static': {
-		    'tools.staticdir.on': True,
-		    'tools.staticdir.dir': 'images'
-		},
-		'/js': {
-		    'tools.staticdir.on': True,
-		    'tools.staticdir.dir': 'js'
-		}
-	}
-	cherrypy.quickstart(Main(), '/', conf)
+cherrypy.config.update("main.conf")
+cherrypy.server.unsubscribe()
+cherrypy.engine.start()
+
+wsgiapp = cherrypy.tree.mount(Root())
